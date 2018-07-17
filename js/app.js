4fcc74da39a743b68c8288b3b012323d3429a6b9
Cody Smith <cms287@Codys-MacBook-Pro.local>
2018-07-16 20:01:11 -0700

    day 4 class work

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let isAdult = confirm('Are you over the age of 18 years old?');
+
+if(isAdult) {
+  let isUnder80 = confirm ("Are you under the age of 80 years old?");
+  if(isUnder80) {
+    let isCool = confirm("Do you like Star Wars?");
+      if(!isCool);
+      alert('You are so cool!');
+  } else {
+    let isElderly = confirm("Do you enjoy prunes?");
+  }
+} else {
+    alert('You were shot first!!');;
+}
