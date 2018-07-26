6a9fbb0e705bdbc91aadd97dcb2b120f21e7ac55
Cody Smith <cms287@gmail.com>
2018-07-25 20:12:39 -0700

    day7 after fizzbuzz

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
  // DAY 7 - MORE DOM STUFF!!!!
 
- const blah = "http://www.cheatsheet.com/wp-content/uploads/2016/03/Yoda-in-Star-Wars-The-Empire-Strikes-Back.jpg"
- let image =
- document.querySelector("img");
+ // const blah = "http://www.cheatsheet.com/wp-content/uploads/2016/03/Yoda-in-Star-Wars-The-Empire-Strikes-Back.jpg"
+ // let image =
+ // document.querySelector("img");
 
 
- image.addEventListener('click', (e) => {
-   image.setAttribute("src", blah);
- })
+ // image.addEventListener('click', (e) => {
+ //   image.setAttribute("src", blah);
+ // });
 
  // const div = document.querySelector('.myDiv');
 
@@ -36,16 +36,55 @@
 // newP.classList.add("purple-text", "green-bg");
 // div.appendChild(newP);
 
+// let newH1 = document.createElement("h1");
+// newH1.textContent = "Hello World";
+// newH1.className = "text-center";
+// div.appendChild(newH1);
 
-let span = document.querySelector("span");
-span.addEventListener("mouseenter", (e) => {
-  span.classList.remove("white-text");
-});
 
-span.addEventListener("click", (e) => {
-  span.classList.toggle("giant-text");
-  span.classList.contains("green-bg") ? // <= Ternary // contains returns a boolean (true/false)
-  span.classList.replace("green-bg", "blue-bg")
-  :
-  span.classList.replace("blue-bg", "green-bg")
-});
+// let span = document.querySelector("span");
+// span.addEventListener("mouseenter", (e) => {
+//   span.classList.remove("white-text");
+// });
+
+// span.addEventListener("click", (e) => {
+//   span.classList.toggle("giant-text");
+//   span.classList.contains("green-bg") ? // <= Ternary // contains returns a boolean (true/false)
+//   span.classList.replace("green-bg", "blue-bg")
+//   :
+//   span.classList.replace("blue-bg", "green-bg")
+// });
+
+// Alright, let's put all this stuff we've learned together:
+//
+// Write a program that prints on the page all numbers from 1 to 100. For multiples of three print “Fizz” instead of the number. For multiples of five print “Buzz” instead of the number. For numbers which are multiples of both three and five print “FizzBuzz”.
+//
+// What you'll need:
+//
+// For loop
+// Modulo/modulus/modsy-whatsit
+// console.log()
+// createElement()
+// appendChild()
+// .innerText
+// if statements or switch statements(lots of ways to do the same thing!)
+
+
+  // FizzBuzz
+  
+  let body = document.querySelector("body");
+  let script = document.querySelector("script");
+
+  for (let i = 1; i <= 100; i++) {
+    let newP = document.createElement('p');
+    if (i % 3 === 0 && i % 5 === 0) {
+      newP.textContent = "FizzBuzz";
+    } else if ( i % 3 === 0) {
+      newP.textContent = "Fizz";
+    } else if (i % 5 === 0) {
+      newP.textContent = "Buzz";
+    } else {
+      newP.textContent = i;
+    }
+    body.insertBefore(newP, script);
+};
